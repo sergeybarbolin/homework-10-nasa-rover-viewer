83a284642c1b3c97dc653c1255c52caa212321ba
sergeybarbolin <sergeybarbolin25@gmail.com>
2019-09-03 21:32:32 +0300

    nasa api

--- src/modules/RoverPhotos/RoverPhotos.js
+++ src/modules/RoverPhotos/RoverPhotos.js
@@ -1,2 +1,80 @@
-// Реализуйте редьюсер
-// Файл с тестами RoverPhotos.test.js поможет вам в этом
+import { combineReducers } from 'redux';
+import { handleActions } from 'redux-actions';
+import { fetchPhotosRequest, fetchPhotosSuccess, fetchPhotosFailure, changeSol } from './actions';
+
+const sol = handleActions(
+    {
+        [changeSol]: (_state, action) => ({
+            current: action.payload,
+            min: 1,
+            max: 100
+        })
+    },
+    {
+        current: 1,
+        min: 1,
+        max: 100
+    }
+)
+
+const photos = handleActions(
+    {
+        [fetchPhotosRequest]: (_state, action) => {
+            const { name, sol } = action.payload
+
+            return {
+                ..._state,
+                [name]: {
+                    ..._state[name],
+                    [sol]: {
+                        photos: [],
+                        isLoading: true,
+                        isLoaded: false
+                    }
+                }
+            }
+        },
+
+        [fetchPhotosSuccess]: (_state, action) => {
+            const { photos, name, sol } = action.payload;
+
+            return {
+                ..._state,
+                [name]: {
+                    ..._state[name],
+                    [sol]: {
+                        photos,
+                        isLoading: false,
+                        isLoaded: true
+                    }
+                }
+            }
+        },
+
+        [fetchPhotosFailure]: (_state, action) => {
+            const { name, sol } = action.payload;
+
+            return {
+                ..._state,
+                [name]: {
+                    ..._state[name],
+                    [sol]: {
+                        photos: [],
+                        isLoading: false,
+                        isLoaded: true
+                    }
+                }
+            }
+        }
+    }, 
+    {
+        curiosity: {},
+        opportunity: {},
+        spirit: {}
+    }
+);
+
+export const rovers = ['curiosity', 'opportunity', 'spirit']
+export const getSol = state => state.roverPhotos.sol
+export const getPhotos = state => state.roverPhotos.photos
+export default combineReducers({ sol, photos })
--- src/modules/RoverPhotos/sagas.js
+++ src/modules/RoverPhotos/sagas.js
@@ -1,1 +1,25 @@
-// Реализуйте саги
+import { takeEvery, select, put, call, fork } from 'redux-saga/effects';
+import { fetchPhotosRequest, fetchPhotosSuccess, fetchPhotosFailure, changeSol } from './actions';
+import { getPhotos } from './api';
+import { getApiKey } from './../Auth';
+
+function* fetchPhotos() {
+    yield takeEvery(fetchPhotosRequest, fetchPhotosFlow)
+}
+
+export function* fetchPhotosFlow(action) {
+    const apiKey = yield select(getApiKey)
+    const { sol, name } = action.payload
+    const { photos } = yield call(getPhotos, apiKey, name, sol)
+
+    try {
+        yield put(fetchPhotosSuccess({ photos, name, sol }))
+        yield put(changeSol( sol ))
+    } catch ({ message }) {
+        yield put(fetchPhotosFailure(message))
+    }
+}
+
+export default function*() {
+    yield fork( fetchPhotos )
+}
